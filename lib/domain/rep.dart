4bada52bbf3b46b8b39b046d8995fdc3149c7e4e
Marta Mirely Nascimento dos Santos <103062784+martanascimento1@users.noreply.github.com>
2022-09-30 09:08:30 -0300

    Update rep.dart

--- lib/domain/rep.dart
+++ lib/domain/rep.dart
@@ -1,16 +1,9 @@
 class Figurinhas {
   final String imagem;
   final String titulo;
-  final String autor;
-  final String paginas;
-  final String opniao;
 
   Figurinhas({
     required this.imagem,
     required this.titulo,
-    required this.autor,
-    required this.paginas,
-    required this.opniao,
-
   });
 }
