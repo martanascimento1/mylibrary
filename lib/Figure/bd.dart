607383a7138e020ed2f3fe7475fc7fcdee882488
Marta Mirely Nascimento dos Santos <103062784+martanascimento1@users.noreply.github.com>
2022-09-30 09:07:40 -0300

    Update bd.dart

--- lib/Figure/bd.dart
+++ lib/Figure/bd.dart
@@ -5,51 +5,33 @@ class Bd {
   static List<Figurinhas> lista = [
     Figurinhas(
       imagem:
-      'https://i.pinimg.com/564x/65/b6/9f/65b69f6da9c93f7706f77252e4d6d6e6.jpg',
-      titulo: 'Neymar',
-      autor: 'Harpeer Lee',
-      paginas: '189',
-      opniao: ' muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom v muito bom  ',
+      'https://i.pinimg.com/236x/a8/46/2d/a8462d781540719280d62ca4ee42c537.jpg',
+      titulo: 'Meu Pé de Laranja Lima',
     ),
     Figurinhas(
       imagem:
-      'https://i.pinimg.com/564x/65/b6/9f/65b69f6da9c93f7706f77252e4d6d6e6.jpg',
-      titulo: 'Neymar',
-      autor: 'Harpeer Lee',
-      paginas: '189',
-      opniao: ' muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom v muito bom  ',
+      'https://i.pinimg.com/564x/68/5d/bc/685dbc7ddc580b14839ebeee6cce943a.jpg',
+      titulo: 'O Sol é Para Todos',
     ),
-    Figurinhas(
+     Figurinhas(
       imagem:
-      'https://i.pinimg.com/564x/65/b6/9f/65b69f6da9c93f7706f77252e4d6d6e6.jpg',
-      titulo: 'Neymar',
-      autor: 'Harpeer Lee',
-      paginas: '189',
-      opniao: ' muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom v muito bom  ',
+      'https://i.pinimg.com/564x/c8/ac/5e/c8ac5e37891974d480e4398a188d297d.jpg',
+      titulo: 'A menina que Roubava Livros',
     ),
-    Figurinhas(
+     Figurinhas(
       imagem:
-      'https://i.pinimg.com/564x/65/b6/9f/65b69f6da9c93f7706f77252e4d6d6e6.jpg',
-      titulo: 'Neymar',
-      autor: 'Harpeer Lee',
-      paginas: '189',
-      opniao: ' muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom v muito bom  ',
+      'https://i.pinimg.com/564x/06/d5/06/06d506602dc36f5bd31a167d15ba7a4d.jpg',
+      titulo: 'Orgulho e Preconceito',
     ),
-    Figurinhas(
+   Figurinhas(
       imagem:
-      'https://i.pinimg.com/564x/65/b6/9f/65b69f6da9c93f7706f77252e4d6d6e6.jpg',
-      titulo: 'Neymar',
-      autor: 'Harpeer Lee',
-      paginas: '189',
-      opniao: ' muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom v muito bom  ',
+      'https://i.pinimg.com/564x/86/bf/bd/86bfbd30ddc7702d58b46a9b8b3caaf5.jpg',
+      titulo: '1984',
     ),
-    Figurinhas(
+     Figurinhas(
       imagem:
-      'https://i.pinimg.com/564x/65/b6/9f/65b69f6da9c93f7706f77252e4d6d6e6.jpg',
-      titulo: 'Neymar',
-      autor: 'Harpeer Lee',
-      paginas: '189',
-      opniao: ' muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom muito bom v muito bom  ',
+      'https://i.pinimg.com/564x/26/d1/45/26d1454c1b2b24977c4c8889d2979913.jpg',
+      titulo: 'A Culpa é das Estrelas',
     ),
   ];
 }
